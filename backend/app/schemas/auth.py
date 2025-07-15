6001537fb8f3e7c3c39813d3c3812fa979d8c087
Paul Regnier <paul_m_regnier@yahoo.com>
2025-07-15 21:09:27 +0100

    Updated /schemas.auth.py

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,17 +1,97 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-from datetime import datetime
 
-# EN: Schema for user login via API
+# EN: Schema for user registration request via API
 # BR: Esquema para... através da API
-class Create_Observation(BaseModel):
+class AuthRegisterRequest(BaseModel):
 
-   Observation_Teacher: int 
-   Observation_Class: str
-   Observation_Focus: str
-   Observation_Strengths: Optional[str]
-   Observation_Weaknesses: Optional[str]
-   Observation_Comments: Optional[str]
+   email: EmailStr 
+   school_id: int
+   password: str
+
+# EN: Schema for user registration response via API
+# BR: Esquema para... através da API
+class AuthRegisterResponse(BaseModel):
+
+   id: int
+   email: EmailStr 
+   school_id: int
+   
+   class Config:
+      orm_mode = True
+
+# EN: Schema for login request  via API
+# BR: Esquema para... através da API
+class AuthLoginRequest(BaseModel):
+
+   email: EmailStr 
+   password: str
+   
+# EN: Schema for login response  via API
+# BR: Esquema para... através da API
+class AuthLoginResponse(BaseModel):
+
+   access_token: str 
+   token_type: str="bearer"
+   user: AuthMeResponse
+
+   class Config:
+      orm_mode = True
+
+# EN: Schema to get user details via API
+# BR: Esquema para... através da API
+class AuthMeResponse(BaseModel):
+
+   id: int
+   school_id: int
+   first_name: Optional[str] = None
+   last_name: Optional[str] = None
+   email: EmailStr 
+   user_role: int
+   is_active: bool
+   
+   class Config:
+      orm_mode = True
+
+# EN: Schema to get request logout via API
+# BR: Esquema para... através da API
+class AuthLogoutResponse(BaseModel):
+
+   detail: str="Successfully logged out!"
+      
+   class Config:
+      orm_mode = True
+
+# EN: Schema for password reset request  via API
+# BR: Esquema para... através da API
+class AuthResetRequest(BaseModel):
+
+   email: EmailStr 
+
+# EN: Schema for password reset response  via API
+# BR: Esquema para... através da API
+class AuthResetResponse(BaseModel):
+
+   detail: str="If the email exists, a password reset link has been sent."
+
+   class Config:
+      orm_mode = True
+
+# EN: Schema for complete password reset (request)  via API
+# BR: Esquema para... através da API
+class AuthCompleteResetRequest(BaseModel):
+
+   token: str
+   new_password: str 
+
+# EN: Schema for complete password reset (response)  via API
+# BR: Esquema para... através da API
+class AuthCompleteResetResponse(BaseModel):
+
+   detail: str="Your password has been changed."
+   access_token: str
+   token_type: str="bearer"
+   user: AuthMeResponse
 
    class Config:
       orm_mode = True
