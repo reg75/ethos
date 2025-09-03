7bc6e7775d402cb932e05a547ffcc69a74ff7072
reg75 <paul_m_regnier@yahoo.com>
2025-09-03 06:47:49 +0100

    Updated verify_token function to lock row

--- backend/app/services/token_service.py
+++ backend/app/services/token_service.py
@@ -1,100 +1,98 @@
 from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
-from app.models.user import Token, User 
+from app.models.user import Token, User, TokenPurposeEnum 
 from app.utils import generate_token, hash_token
 from fastapi import HTTPException, status
 
 def create_token(db, user_id, purpose, expiry_minutes):
-   """
-   EN: Creates and stores a secure token for a specific purpose (e.g. email verification).
-   BR: Cria e armazena um token seguro para um propósito específico (ex: verificação de e-mail).
-   """   
+    """
+    EN: Creates and stores a secure token for a specific purpose (e.g. email verification).
+    BR: Cria e armazena um token seguro para um propósito específico (ex: verificação de e-mail).
+    """   
    
-   now = datetime.now(timezone.utc) #Gets current time
+    now = datetime.now(timezone.utc) #Gets current time
 
-   raw_token = generate_token() # Sent via email
+    raw_token = generate_token() # Sent via email
 
-   hashed_token = hash_token(raw_token) # Stored in db
+    hashed_token = hash_token(raw_token) # Stored in db
 
-   token_row = Token(
-      user_id=user_id,
-      token_hash = hashed_token,
-      purpose=purpose,
-      created_at=now,
-      expires_at=now + timedelta(minutes=expiry_minutes),
-      used=False)
+    token_row = Token(
+        user_id=user_id,
+        token_hash = hashed_token,
+        purpose=purpose,
+        created_at=now,
+        expires_at=now + timedelta(minutes=expiry_minutes),
+        used=False)
 
-   # EN: Add token to database
-   try:
-      db.add(token_row)
-      db.commit()
-      db.refresh(token_row)
-      return token_row,  raw_token
+    # EN: Add token to database
+    try:
+        db.add(token_row)
+        db.commit()
+        db.refresh(token_row)
+        return token_row,  raw_token
    
-   except Exception as e:
-      db.rollback()
-      raise e
+    except Exception as e:
+        db.rollback()
+        raise e
 
 
 def create_verification_token(db: Session, user_id: int):
-   """
-   EN: Creates and stores a secure token for email verification.
-   BR: Cria e armazena um token seguro para verificação de e-mail.
-   """
-   expiry_minutes = 4320 # 3 days / 3 dias
+    """
+    EN: Creates and stores a secure token for email verification.
+    BR: Cria e armazena um token seguro para verificação de e-mail.
+    """
+    expiry_minutes = 4320 # 3 days / 3 dias
    
-   return create_token(db, user_id, "CONFIRM_EMAIL", expiry_minutes)
+    return create_token(db, user_id, TokenPurposeEnum.CONFIRM_EMAIL, expiry_minutes)
 
 
 def create_password_reset_token(db: Session, user_id: int):
-   """
-   EN: Creates and stores a secure token for resetting a password.
-   BR: Cria e armazena um token seguro para redefinição de senha.
-   """
-   expiry_minutes = 60 # One hour / uma hora
+    """
+    EN: Creates and stores a secure token for resetting a password.
+    BR: Cria e armazena um token seguro para redefinição de senha.
+    """
+    expiry_minutes = 60 # One hour / uma hora
    
-   return create_token(db, user_id, "PASSWORD_RESET", expiry_minutes)
+    return create_token(db, user_id, TokenPurposeEnum.PASSWORD_RESET, expiry_minutes)
 
-def verify_token(db: Session, token_str: str, expected_purpose: str) -> Token:
-   """
-   EN: Verifies a token's validity, expiry, and purpose. Returns token if valid.
-   BR: Verifica validade, expiração e propósito de um token. Retorna o token se válido.
-   """   
-   
-   token_hash = hash_token(token_str)
+def verify_token(db: Session, token_str: str, expected_purpose: TokenPurposeEnum) -> Token:
+    """
+    EN: Verifies a token's validity, expiry, and purpose. Returns token if valid.
+    BR: Verifica validade, expiração e propósito de um token. Retorna o token se válido.
+    """   
+    now = datetime.now(timezone.utc)
+    
+    token_hash = hash_token(token_str)
 
-   token = db.query(Token).filter_by(token_hash=token_hash).first()
+    token = (
+        db.query(Token)
+        .filter_by(token_hash=token_hash, 
+                   purpose=expected_purpose)
+        .with_for_update()
+        .first()
+    )
+
+    # Checks if it is a token
+    if not token or token.used or token.expires_at < now:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token!")
    
-   # Checks if it is a token
-   if not token:
-      raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid token")
-   
-   if token.used:
-      raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="This token has been used")
-   
-   # Checks if token has expired
-   if token.expires_at < datetime.now(timezone.utc):
-      raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="This token has expired")
-   
-   if expected_purpose != token.purpose:
-      raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Invalid token purpose")
-   
-   return token
+    return token
 
 def verify_verification_token(db: Session, verification_token: str):
-   """
-   EN: Verifies the validity, expiry, correct purpose of an email verification token. Returns token if valid.
-   BR: Verifica validade, expiração e propósito correto de um token de verificação de e-mail. Retorna o token se válido.
-   """  
-   expected_purpose = "CONFIRM_EMAIL"
+    """
+    EN: Verifies the validity, expiry, correct purpose of an email verification token. Returns token if valid.
+    BR: Verifica validade, expiração e propósito correto de um token de verificação de e-mail. Retorna o token se válido.
+    """  
+    expected_purpose = TokenPurposeEnum.CONFIRM_EMAIL
    
-   return verify_token(db=db, token_str=verification_token, expected_purpose=expected_purpose)
+    return verify_token(db=db, token_str=verification_token, expected_purpose=expected_purpose)
 
 def verify_password_reset_token(db: Session, verification_token: str):
-   """
-   EN: Verifies the validity, expiry, correct purpose of a password reset token. Returns token if valid.
-   BR: Verifica validade, expiração e propósito correto de um token de redefinição de senha. Retorna o token se válido.
-   """  
-   expected_purpose = "PASSWORD_RESET"
+    """
+    EN: Verifies the validity, expiry, correct purpose of a password reset token. Returns token if valid.
+    BR: Verifica validade, expiração e propósito correto de um token de redefinição de senha. Retorna o token se válido.
+    """  
+    expected_purpose = TokenPurposeEnum.PASSWORD_RESET
    
-   return verify_token(db=db, token_str=verification_token, expected_purpose=expected_purpose)
+    return verify_token(db=db, token_str=verification_token, expected_purpose=expected_purpose)
+
